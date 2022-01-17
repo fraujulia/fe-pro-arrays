a8d05e0b96a36fe4011e0b808fa9dad71af95799
yulia tertyshnyk <yulia.tertyshnyk@gmail.com>
2022-01-17 16:21:33 +0200

    added changes

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -9,6 +9,7 @@ function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
     newArray.push(callback(array[i], i, array));
   }
+  return newArray
 }
 
 /* Функция принимает в себе первым аргументом массив, вторым функцию которая будет вызываться для каждого элемента массива
